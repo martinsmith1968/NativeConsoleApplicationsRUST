83ea70d93cc2eea27fd29e03ee71677230b7fce9
Martin Smith <martinsmith1968@gmail.com>
2026-02-22 22:46:49 +0000

    After rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
-use uuid::Uuid;
 use nanoid::nanoid;
-use strfmt::strfmt;
 use std::collections::HashMap;
+use strfmt::strfmt;
+use uuid::Uuid;
 
 #[derive(clap::ValueEnum, Clone, Parser, Debug, PartialEq, Copy)]
 #[clap(rename_all = "kebab-case")]
@@ -14,15 +14,20 @@ enum GuidVersionType {
 
 #[derive(clap::ValueEnum, Clone, Parser, Debug, PartialEq, Copy)]
 #[clap(rename_all = "lowercase")]
-enum UUIDType
-{
+enum UUIDType {
     Guid,
-    NanoId
+    NanoId,
 }
 
 /// Generate Unique IDs (UUIDs) with controlled output and formatting
 #[derive(Parser, Debug)]
-#[command(version, about, author, help_expected = true, after_help = "NOTE:\noutput-template supports: {uuid}, {sequence} dynamic values\n(See also : https://github.com/vitiral/strfmt)")]
+#[command(
+    version,
+    about,
+    author,
+    help_expected = true,
+    after_help = "NOTE:\noutput-template supports: {uuid}, {sequence} dynamic values\n(See also : https://github.com/vitiral/strfmt)"
+)]
 struct Args {
     /// Number of times to generate
     #[arg(short = 'c', long, default_value_t = 1)]
@@ -70,8 +75,7 @@ struct GuidGenerateOptions {
 fn main() {
     let args = Args::parse();
 
-    for sequence in 1..=args.count
-    {
+    for sequence in 1..=args.count {
         let uuid_formatted = generate_uuid(&args);
         let output = format_output(&args.output_template, &uuid_formatted, sequence);
 
@@ -79,18 +83,14 @@ fn main() {
     }
 }
 
-fn generate_uuid(args: &Args) -> String
-{
-    if args.uuid_type == UUIDType::Guid
-    {
-        let format_options = FormatOptions
-        {
+fn generate_uuid(args: &Args) -> String {
+    if args.uuid_type == UUIDType::Guid {
+        let format_options = FormatOptions {
             hyphenated: !args.non_hyphenated,
             uppercase: args.uppercase,
         };
 
-        let generate_options = GuidGenerateOptions
-        {
+        let generate_options = GuidGenerateOptions {
             guid_version: args.guid_version,
             v6_seed: args.guid_v6_seed.clone(),
         };
@@ -100,8 +100,8 @@ fn generate_uuid(args: &Args) -> String
 
         return uuid_formatted.clone();
     }
-    else if args.uuid_type == UUIDType::NanoId
-    {
+
+    if args.uuid_type == UUIDType::NanoId {
         let uuid = generate_nanoid(args.nanoid_length);
 
         return uuid.clone();
@@ -110,16 +110,11 @@ fn generate_uuid(args: &Args) -> String
     String::new()
 }
 
-fn generate_guid(options: GuidGenerateOptions) -> Uuid
-{
+fn generate_guid(options: GuidGenerateOptions) -> Uuid {
     if options.guid_version == GuidVersionType::V6 {
         let node = &[1, 2, 3, 4, 5, 6];
 
         if !options.v6_seed.is_empty() {
-            //let seed_values = options.v6_seed.split(",")
-            //    .map(|str| str.trim().parse::<u8>().unwrap_or_default())
-            //    .collect::<Vec<u8>>();
-
             let mut seed_values: Vec<u8> = Vec::new();
             let mut invalid_values: Vec<&str> = Vec::new();
 
@@ -133,13 +128,18 @@ fn generate_guid(options: GuidGenerateOptions) -> Uuid
             if seed_values.len() == 6 {
                 let _node: &[u8] = &seed_values;
             }
+            
             if invalid_values.len() > 0 {
-                println!("Warning: unable to use seed values - {}", invalid_values.join(", "));
+                println!(
+                    "Warning: unable to use seed values - {}",
+                    invalid_values.join(", ")
+                );
             }
         }
 
         return Uuid::now_v6(node);
     }
+
     if options.guid_version == GuidVersionType::V7 {
         return Uuid::now_v7();
     }
@@ -147,29 +147,22 @@ fn generate_guid(options: GuidGenerateOptions) -> Uuid
     Uuid::new_v4()
 }
 
-fn generate_nanoid(nanoid_length: usize) -> String
-{
+fn generate_nanoid(nanoid_length: usize) -> String {
     let uuid = nanoid!(nanoid_length);
 
     uuid
 }
 
-fn format_guid(uuid: &Uuid, format_options: &FormatOptions) -> String
-{
-    let value: String = if format_options.hyphenated
-    {
+fn format_guid(uuid: &Uuid, format_options: &FormatOptions) -> String {
+    let value: String = if format_options.hyphenated {
         uuid.hyphenated().to_string()
-    }
-    else
-    {
+    } else {
         uuid.simple().to_string()
     };
 
-    let value = if format_options.uppercase
-    {
+    let value = if format_options.uppercase {
         value.to_uppercase()
-    }
-    else {
+    } else {
         value.to_lowercase()
     };
 
@@ -185,7 +178,7 @@ fn format_output(output_format: &String, formatted_uuid: &String, sequence: u8) 
     vars.insert("sequence".to_string(), sequence.to_string());
     vars.insert("uuid".to_string(), formatted_uuid.to_string());
 
-    let result  = strfmt(&output_format, &vars).unwrap();
+    let result = strfmt(&output_format, &vars).unwrap();
 
     result
 }
